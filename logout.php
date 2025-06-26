3553e66459f66049bf13e7c88e6a4a684d442f4c
athuull <athulvanil03@gmail.com>
2025-06-26 11:43:50 +0530

    comments

--- logout.php
+++ logout.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
+session_unset(); // unsets all the session variables 
+session_destroy();  // destroys the session
 
+
+// JS pop up and redirect
 echo "<script>
     alert('You have been logged out.');
-    window.location.href = 'auth/login.php'; // or wherever you want to send them
+    window.location.href = 'auth/login.php'; 
 </script>";
 exit();
 ?>
